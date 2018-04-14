86097f860f06f66d3964fb1caebf752041a9229c
Azarien <Azarien@users.noreply.github.com>
2018-04-14 10:31:31 +0200

    Enable visual styles.

--- src/winfile.c
+++ src/winfile.c
@@ -1319,3 +1319,10 @@ bDialogMessage(PMSG pMsg)
    return FALSE;
 }
 
+#pragma comment(linker, "\"/manifestdependency: \
+    type = 'Win32'                              \
+    name = 'Microsoft.Windows.Common-Controls'  \
+    version = '6.0.0.0'                         \
+    processorArchitecture = '*'                 \
+    publicKeyToken = '6595b64144ccf1df'         \
+    language = '*'                              \"")
