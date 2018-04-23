6fc975414d6542aa3bf4a849aaf26d0fc63a936c
mattn <mattn.jp@gmail.com>
2018-04-23 10:12:31 +0900

    enable visual style on mingw (#104)

--- src/winfile.c
+++ src/winfile.c
@@ -13,14 +13,6 @@
 #include <commctrl.h>
 #include <shlobj.h>
 
-#pragma comment(linker, "\"/manifestdependency: \
-    type = 'Win32'                              \
-    name = 'Microsoft.Windows.Common-Controls'  \
-    version = '6.0.0.0'                         \
-    processorArchitecture = '*'                 \
-    publicKeyToken = '6595b64144ccf1df'         \
-    language = '*'                              \"")
-
 //
 // Overall Window structure
 //
