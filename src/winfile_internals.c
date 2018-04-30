832bb17a31493b1815ad1b7dba45923d8f05a055
dbjdbj <dbj@dbj.org>
2018-04-30 11:58:13 +0200

    after much non trivial research

--- src/winfile_internals.c
+++ src/winfile_internals.c
@@ -2,27 +2,44 @@
 2018-04-30	dbj@dbj.org		created
 */
 #include <windows.h>
-#include <ntddkbd.h>
+// #include <ntdef.h>
 #include "winfile_internals.h"
 
 /// <summary>
 /// non-linguistic byte level comparison of strings
-/// note: RtlCompare[Unicode]String is called from CompareString[Ex]
+/// note: RtlCompare[Unicode]String is now part of WDK and is thus
+/// not a feasible solution for legacy code
+/// Thus what seems as a cludge is to use CompareString in a locale invariant mode
+/// alernative is to develop a solution using strcmp() and friends ...
+/// So this is not a ordinal comparison but only for ASCI build of winfile
+/// which is if not non existent than very rare variant of winfile
 /// </summary>
-int winfile_exact_string_compareA(const char * str1, const char * str2, unsigned char CaseInSensitive) {
-	return RtlCompareString(
-		str1, str2, CaseInSensitive
-	);
+int winfile_ordinal_string_compareA(const char * str1, const char * str2, unsigned char ignore_case) {
+
+	return CompareStringA(
+		/* _In_ LCID    */ LOCALE_INVARIANT,
+		/* _In_ DWORD   */ ignore_case == 1 ? LINGUISTIC_IGNORECASE : NORM_LINGUISTIC_CASING,
+		/* _In_ LPCTSTR */ str1,
+		/* _In_ int     */ -1,
+		/* _In_ LPCTSTR */ str2,
+		/* _In_ int     */ -1);
 }
 
-int winfile_exact_string_compareW(const wchar_t * str1, const wchar_t * str2, unsigned char CaseInSensitive) {
-	return RtlCompareUnicodeString(
-		str1, str2, CaseInSensitive
+/// <summary>
+/// CompareStringOrdinal simply does not exist for asci strings
+/// which is telling, in the context of one will never
+/// need it, because one will not build non unicode apps 
+/// from XP onwards that is
+/// </summary>
+int winfile_ordinal_string_compareW(const wchar_t * str1, const wchar_t * str2, unsigned char ignore_case) {
+	return CompareStringOrdinal(
+		str1, -1, str2, -1, ignore_case
 	);
 }
 
 /// <summary>
-/// this is legacy aka "pre vista" solution 
+/// This is to be used for "UI sting comparisons" or sorting and a such
+/// this is locale sensitive, legacy aka "pre vista" solution 
 /// when locales had ID's not names
 /// and when CompareStringEx was not
 /// </summary>
