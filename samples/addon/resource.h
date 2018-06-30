3657bc46201d5c67c2cb3047c79e2ff861b5fa56
BaldwinTechnology <38319355+BaldwinTechnology@users.noreply.github.com>
2018-06-29 23:48:16 -0700

    Add WinFile Addon Sample (from BaldwinTechnology).

---
+++ samples/addon/resource.h
@@ -0,0 +1,29 @@
+/********************************************************************
+
+   resource.h
+
+   Windows File Manager Sample Addon resource definitions
+
+   Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License.
+
+********************************************************************/
+
+#define IDR_MENU                        101
+#define IDB_TEST                        102
+
+#define IDM_FIRSTBUTTON 1
+#define IDM_TESTMENU    2
+#define IDM_TOGGLE      8
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
