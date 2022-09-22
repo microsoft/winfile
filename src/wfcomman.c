0d7644f31b0790d1ac5badd1abe0f7816ae25cde
Hermann Schinagl <hermann.schinagl@avl.com>
2022-09-22 06:41:55 +0200

    Revert "szPath too short because CreateDirWindow() assumes a filename can be the same length as MAXPATHLEN when it appends"
    This reverts commit 44991cb748c58e413f22ce621c5454a67369acce.

--- src/wfcomman.c
+++ src/wfcomman.c
@@ -604,7 +604,7 @@ OpenOrEditSelection(HWND hwndActive, BOOL fEdit)
    DWORD ret;
    HCURSOR hCursor;
 
-   WCHAR szPath[MAXPATHLEN * 2 + 2];  // +2 for quotes if needed, * 2 because CreateDirWindow() assumes a filename can be the same length as MAXPATHLEN
+   WCHAR szPath[MAXPATHLEN+2];  // +2 for quotes if needed
 
    HWND hwndTree, hwndDir, hwndFocus;
 
